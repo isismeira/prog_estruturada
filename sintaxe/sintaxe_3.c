c9cce04985bc7ef74e1b382d70d7c191ff30f013
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-03-12 16:49:24 -0300

    Commit

---
+++ sintaxe/sintaxe_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() 
+{
+    int inteiro = 10;
+    float flutuante = 10.5;
+    char caractere = 'a';
+
+    printf("%i\n", inteiro);
+    printf("%f\n", flutuante);
+    printf("%c\n", caractere);
+
+    return 0;
+}
---
+++ sintaxe/sintaxe_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int contador;
+    int numero = 1;
+
+    // Estrutura de repetição for
+    for (contador = 1; contador <= 10; contador++)
+    {
+        printf("Contando... %i\n", numero);
+        numero++;
+    }
+
+
+    printf("Fim da contagem progressiva\n");
+    printf("Inicio da contagem regressiva\n");
+
+     // Estrutura de repetição while
+    while (contador>=2){
+        numero--;
+        printf("Contando... %i\n", numero);
+        contador--;
+    }
+
+    return 0;
+}
