000c01b0f0765797522a60845550febb072b59e0
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-03-12 14:16:04 -0300

    Initial commit

---
+++ sintaxe.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclui a bibliotea padrão de entrada e saída
+ 
+int main() { // Função principal
+    int idade = 19; // Declaração de variável inteira
+    printf("Hello World!!!"); 
+    return 0;  // Retorna 0 para o sistema operacional
+}
