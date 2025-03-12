2e5f5ece4f125f6bcbc69eee9e7d034aafee7235
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-03-12 14:33:45 -0300

    Commit

--- sintaxe.c
+++ sintaxe.c
@@ -1,7 +1,17 @@
 #include <stdio.h> // Inclui a bibliotea padrão de entrada e saída
  
 int main() { // Função principal
+
     int idade = 19; // Declaração de variável inteira
-    printf("Hello World!!!"); 
+    int idadePai = 50;
+    int idadeMae = 49;  
+
+    printf("Hello World!!!\n");
+
+    printf("Minha idade eh %i anos\n", idade); // Exibindo conteúdo da variável
+
+    printf("A idade do meu pai eh %i anos e a idade da minha mae eh %i anos\n",
+            idadePai, idadeMae); //Exibindo conteúdo de mais de uma variável
+     
     return 0;  // Retorna 0 para o sistema operacional
 }
